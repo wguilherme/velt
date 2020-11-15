304cfb16165e32c16eb1e715410a0532e4e3c48e
W. Guilherme <ravok123@gmail.com>
2020-11-15 19:48:47 -0300

    [Update] Update login route and component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { LoginComponent } from './pages/login/login.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: "dashboard", pathMatch: "full" },
+  { path: '', redirectTo: "login", pathMatch: "full" },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'company/:id', pathMatch: 'full', component: CompanyDetailComponent },
